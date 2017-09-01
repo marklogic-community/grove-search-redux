50331365a0f684b9a37efeede91c2efaa3db4895
Patrick McElwee <pmcelwee@gmail.com>
2017-09-01 15:30:14 -0400

    Add umd, which let my React file pick it up

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ml-search-redux',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
 		rules: [
